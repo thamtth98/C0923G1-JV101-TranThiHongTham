1594b7e416480863c1e6460fd0f48a957a03c2e5
hongthamtt <tranthihongtham1604@gmail.com>
2023-12-27 08:17:26 +0700

    ls10

--- module_3/Lesson10/calculator/src/main/java/com/example/servlet/CalculatorServlet.java
+++ module_3/Lesson10/calculator/src/main/java/com/example/servlet/CalculatorServlet.java
@@ -1,11 +1,14 @@
 package com.example.servlet;
 
+import com.example.calculator.Calculator;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name = "CalculatorServlet", value = "/calculator")
 public class CalculatorServlet extends HttpServlet {
@@ -20,6 +23,15 @@ public class CalculatorServlet extends HttpServlet {
         double secondNumber = Double.parseDouble(req.getParameter("second-number"));
         char operator = req.getParameter("operator").charAt(0);
 
-        
+        PrintWriter writer = resp.getWriter();
+        writer.println("<html>");
+        writer.println("<h1>Result:</h1>");
+        try{
+            double result = Calculator.calculate(firstNumber, secondNumber, operator);
+            writer.println(firstNumber + " " + operator + " " + secondNumber + " = " + result);
+        }catch (Exception e){
+            writer.println("Error: " + e.getMessage());
+        }
+        writer.println("</html>");
     }
 }
--- module_3/apache-tomcat-9.0.84/webapps/ROOT/calculator.jsp
+++ module_3/apache-tomcat-9.0.84/webapps/ROOT/calculator.jsp
@@ -6,11 +6,11 @@
 </head>
 <body>
 <h3>Simple Calculator</h3>
-<form action="/calculator" method="post">
+<form action="calculator" method="post">
 <table>
     <tr>
         <td>First operand: </td>
-        <td><input placeholder="first number" type="number"></td>
+        <td><input name="first-number" type="number"></td>
     </tr>
     <tr>
         <td>Operator: </td>
@@ -25,7 +25,7 @@
     </tr>
     <tr>
         <td>Second operand: </td>
-        <td><input placeholder="second number" type="number"></td>
+        <td><input name="second-number" type="number"></td>
     </tr>
     <tr>
         <td></td>
